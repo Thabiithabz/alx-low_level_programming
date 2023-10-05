c2be5438ebb062140cacd40182d22dd796af5466
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-10-05 14:08:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - this prints exactly the input on it
- * Return: Always 1
+ * main - main function
+ *
+ * Return: 1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	return (1);
+	return(1);
 }
