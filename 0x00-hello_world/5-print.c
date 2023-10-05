657b111d946a49c866ec76df2e8be6d8d1e6bbd0
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-10-05 13:51:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - thsis is the main functions
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,")
+}
