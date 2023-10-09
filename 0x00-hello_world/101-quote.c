0d890b30e4556807cddb6dfa0904ccf474b81208
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-10-09 05:19:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main function
- * Return: 1
+ * main - A c program that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19", 59);
+	fputs("And that piece of art is useful - Dora Korpar 2015-10-19\n", stdout);
 	return (1);
 }
