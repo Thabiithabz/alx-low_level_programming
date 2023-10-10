8f322df7c136e3934c91560208dddc6abe3bf2ef
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-10-10 13:30:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,5 @@
 /**
  * _islower - Checks the lowere case character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * Return: 1 if c is a letter, 0 otherwise
  */
-int _islower(int c)
-{
-	if (c <= 97 && c <= 122)
-	{
-		return (1);
-	}
-	return (0);
-}
