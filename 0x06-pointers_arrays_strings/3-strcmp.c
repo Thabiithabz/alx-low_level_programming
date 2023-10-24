a9b0470d177a498e5591d70c99c68d24103eed97
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-10-24 13:39:51 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,19 +6,12 @@
  * @s2: a pointer of the second string to be compared
  * Return: 1 if true , 0 if false
  */
-int *_strcmpy(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
-
-	while (*s1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1 != s2)
-		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
-		}
 		s1++;
 		s2++;
 	}
-	return (1);
+	return (*s1 - *s2);
 }
