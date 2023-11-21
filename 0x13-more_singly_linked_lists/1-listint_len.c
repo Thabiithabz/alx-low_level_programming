ec3e78453388f7818c7d1f137e1ed42be3d9744d
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-11-21 12:12:34 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,14 +5,14 @@
  * @h: head pointer
  * return: number of element in list
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t count;
 
 	if (h == NULL)
 		return (0);
 
-	for (count = 0; h != NULL; count++;)
+	for (count = 0; h != NULL; count++)
 	{
 		h = h->next;
 	}
