746d1dd374c5f43b5ddd1463e413001f8128253e
Thabiithabz <thabangmahlatsemalahlela@gmail.com>
2023-11-21 14:28:37 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,28 +4,28 @@
 #include <string.h>
 
 /**
- * print_list - print elements of list_t list
+ * print_list - print all elements of list_t list
  * @h: name of list
  *
- * return: number of nodes in h
+ * return: If str is NULL, print [0] (nil)
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nill)\n");
-		}
-	else
-	{
-		printf("[%d] %s\n", h->len, h->str);
-	}
-		count++;
-		h = h->next;
-	}
-
-	return (count);
+size_t nodes;
+       
+nodes = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
